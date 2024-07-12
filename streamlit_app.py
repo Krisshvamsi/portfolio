9e5ab3384b43ce7af64bfda60864c6c84ed6b287
KRISHNA VAMSI ROKKAM <65437077+Krisshvamsi@users.noreply.github.com>
2024-07-12 10:38:19 -0400

    initial commit

---
+++ streamlit_app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+
+# --- PAGE SETUP ---
+about_page = st.Page(
+    "views/about_me.py",
+    title="About Me",
+    icon=":material/account_circle:",
+    default=True,
+)
+project_1_page = st.Page(
+    "views/projects.py",
+    title="My projects",
+    icon=":material/bar_chart:",
+)
+project_2_page = st.Page(
+    "views/chatbot.py",
+    title="Ask me Anything about Krishna",
+    icon=":material/smart_toy:",
+)
+
+# --- NAVIGATION SETUP [WITH SECTIONS]---
+pg = st.navigation(
+    {
+        "Info": [about_page],
+        "Projects": [project_1_page, project_2_page],
+    }
+)
+
+
+# --- SHARED ON ALL PAGES ---
+st.logo("assets/pic.png")
+st.sidebar.text("Made with ❤️ by Krishna ")
+
+
+# --- RUN NAVIGATION ---
+pg.run()
